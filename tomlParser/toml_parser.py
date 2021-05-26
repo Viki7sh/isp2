38bc9e89cda3198103d9dd91d4bc2d7115f69e1c
Viki7sh <viki7sh@yandex.ru>
2021-05-26 15:42:07 +0300

    add dump toml

--- tomlParser/toml_parser.py
+++ tomlParser/toml_parser.py
@@ -4,6 +4,15 @@ from Utilities.Utilities import convert, deconvert
 
 class Toml:
 
+    def dump(obj, file="testtoml.toml"):
+        packed = convert(obj)
+        with open(file, 'w+') as fw:
+            pytomlpp.dump(packed, fw)
+
+    def dumps(obj):
+        packed = convert(obj)
+        return pytomlpp.dumps(packed)
+
     def load(file="testtoml.toml"):
         with open(file, 'r+') as fr:
             packed = pytomlpp.load(fr)
